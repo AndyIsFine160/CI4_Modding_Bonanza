7f6f4c7a3f8c329ee0d6a3c392fcbdc12caef0a3
HomeLaptop <andyisfine032@gmail.com>
2025-10-02 23:09:06 +0800

    feat: Added a landing page - Added Users.php controller - Updated Routes.php

---
+++ backend/app/Controllers/Users.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function index(): string
+    {
+        return view('user/landing');
+    }
+    public function roadmap(): string
+    {
+        return view('user/roadmap');
+    }
+    public function moodboard(): string
+    {
+        return view('user/moodboard');
+    }
+}
